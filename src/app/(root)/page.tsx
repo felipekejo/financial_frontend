cdac136a455ec7afa2b037d821baa9825ee38b0e
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-05-27 15:05:27 +0930

    start creating header

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,22 @@
+import HeaderBox from '@/components/HeaderBox'
+
 const Home = () => {
-  return <h1>Home</h1>
+  const loggedIn = { firstName: 'Felipe' }
+
+  return (
+    <section className="no-scrollbar flex w-full flex-row max-xl:max-h-screen max-xl:overflow-y-scroll">
+      <div className="no-scrollbar flex w-full flex-1 flex-col gap-8 px-5 sm:px-8 py-7 lg:py-12 xl:max-h-screen xl:overflow-y-scroll">
+        <header className="flex flex-col justify-between gap-8">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn.firstName}
+            subtext="Access and manage your finances"
+          />
+        </header>
+      </div>
+    </section>
+  )
 }
 
 export default Home
