dc8bdeb11685806d1443331bbf6037247b1729bb
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-06-05 15:01:24 +0930

    add fake numbers

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,6 +5,47 @@ import TotalBalanceBox from '@/components/TotalBalanceBox'
 const Home = () => {
   const loggedIn = { firstName: 'Felipe' }
 
+  const accounts = [
+    {
+      id: 'string',
+      availableBalance: 1234,
+      currentBalance: 1234,
+      officialName: 'string',
+      mask: 'string',
+      institutionId: 'string',
+      name: 'string',
+      type: 'string',
+      subtype: 'string',
+      appwriteItemId: 'string',
+      shareableId: 'string',
+    },
+    {
+      id: 'string',
+      availableBalance: 1234,
+      currentBalance: 3454,
+      officialName: 'string',
+      mask: 'string',
+      institutionId: 'string',
+      name: 'string',
+      type: 'string',
+      subtype: 'string',
+      appwriteItemId: 'string',
+      shareableId: 'string',
+    },
+    {
+      id: 'string',
+      availableBalance: 1234,
+      currentBalance: 3454,
+      officialName: 'string',
+      mask: 'string',
+      institutionId: 'string',
+      name: 'string',
+      type: 'string',
+      subtype: 'string',
+      appwriteItemId: 'string',
+      shareableId: 'string',
+    },
+  ]
   return (
     <section className="no-scrollbar flex w-full flex-row max-xl:max-h-screen max-xl:overflow-y-scroll">
       <div className="no-scrollbar flex w-full flex-1 flex-col gap-8 px-5 sm:px-8 py-7 lg:py-12 xl:max-h-screen xl:overflow-y-scroll">
@@ -16,7 +57,7 @@ const Home = () => {
             subtext="Access and manage your finances"
           />
           <TotalBalanceBox
-            accounts={[]}
+            accounts={accounts}
             totalBanks={1}
             totalCurrentBalance={100000}
           />
