55de56ea1cd8c1a369311cd1df61ce2537421b1d
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-06-13 15:02:20 +0930

    handle error form

--- src/app/auth/sign-in/actions.tsx
+++ src/app/auth/sign-in/actions.tsx
@@ -1,14 +1,42 @@
 'use server'
-
 import { sigInWithPassword } from '@/http/sign-in-with-password'
+import { HTTPError } from 'ky'
+import { z } from 'zod'
 
-export async function sigInWithEmailAndPassword(data: FormData) {
-  const { email, password } = Object.fromEntries(data)
+const signInSchema = z.object({
+  email: z
+    .string()
+    .email({ message: 'Please, provide a valid e-mail address.' }),
+  password: z.string().min(1, { message: 'Please, provide a password.' }),
+})
 
-  const result = await sigInWithPassword({
-    email: String(email),
-    password: String(password),
-  })
+export async function sigInWithEmailAndPassword(_: unknown, data: FormData) {
+  const result = signInSchema.safeParse(Object.fromEntries(data))
 
-  console.log(result)
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+    return { success: false, message: null, errors }
+  }
+
+  const { email, password } = result.data
+
+  try {
+    const { token } = await sigInWithPassword({
+      email,
+      password,
+    })
+    console.log(token)
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+      return { success: false, message, errors: null }
+    }
+    return {
+      success: false,
+      message: 'Unexpected error, try again later.',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: null, errors: null }
 }
